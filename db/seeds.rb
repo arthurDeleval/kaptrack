5536981029a7e2d9ccede48577587c9353c49b66
Hicham Elbaamrani <elbaamrani.hicham@gmail.com>
2020-12-02 15:17:03 +0100

    form views for supplier, lots, product // index view for lots

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Supplier.destroy_all
+Product.destroy_all
+Lot.destroy_all
+
+
+
+require 'faker'
+
+puts 'Creating fake data ...'
+10.times do
+  supplier = Supplier.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    delivery_estimation: "#{[2..10].sample} days"
+  )
+  supplier.save!
+end
+
+10.times do
+  product = Product.new(
+    name:    Faker::Food.ingredient,
+    measure_unit: Faker::Food.measurement,
+  )
+  product.save!
+end
+
+puts 'Finished!'
