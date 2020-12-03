1324bca33a986acd819c941b5a629fd44fe6fd86
ValentinLanglet <valentin.langlet59@gmail.com>
2020-12-03 12:07:54 +0100

    cards index checklists ok form new checklists ok et form task ok
    Signed-off-by: ValentinLanglet <valentin.langlet59@gmail.com>

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,6 +1,6 @@
 class Checklist < ApplicationRecord
   has_many :assignments
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
 end
