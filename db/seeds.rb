d2b20b416580831b70f2508b39f700b9143c8333
Hicham Elbaamrani <elbaamrani.hicham@gmail.com>
2020-12-07 13:07:04 +0100

    recipe seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Supplier.destroy_all
-Product.destroy_all
 Lot.destroy_all
+Product.destroy_all
 User.destroy_all
 Recipe.destroy_all
+Supplier.destroy_all
 
 require 'faker'
 
-puts 'Creating fake data ...'
+puts 'Creating suppliers ...'
 100.times do
   supplier = Supplier.new(
     name:    Faker::Company.name,
@@ -24,6 +24,10 @@ puts 'Creating fake data ...'
   )
   supplier.save!
 end
+puts 'suppliers Finished!'
+
+
+puts 'Creating products ...'
 
 100.times do
   product = Product.new(
@@ -33,16 +37,19 @@ end
   product.save!
 end
 
-puts 'Finished!'
-
+puts 'products Finished!'
 
 puts 'Creating users...'
 arthur = User.create!(first_name: "arthur", last_name: "Deleval", email: "arthur@gmail.com", phone_number: 0645353434, role: "manager", password: "azerty")
 puts 'Finished!'
 
+Recipe.create(name: "burger", recipe_description:"Tranchez de fines lamelles d'emmental afin de recouvrir la partie inférieure de votre pain à burger. Mettez ensuite au four sur le mode grill le temps de faire fondre le fromage.", price: 20, cost: 10, meal_description: "burger",preparation_time: 10, user_id: arthur.id)
+Recipe.create(name: "fajitas", recipe_description:"Tranchez de fines lamelles d'emmental afin de recouvrir la partie inférieure de votre pain à burger. Mettez ensuite au four sur le mode grill le temps de faire fondre le fromage.", price: 10, cost: 5, meal_description: "old el paso",preparation_time: 15, user_id: arthur.id)
+Recipe.create(name: "mac&cheese", recipe_description:"Tranchez de fines lamelles d'emmental afin de recouvrir la partie inférieure de votre pain à burger. Mettez ensuite au four sur le mode grill le temps de faire fondre le fromage.", price: 12, cost: 6, meal_description: "cheese",preparation_time: 8, user_id: arthur.id)
+puts "recipe created"
+
 Checklist.create!(title: "Laver les tables", description: "très rapidement")
 Checklist.create!(title: "Faire la vaisselle", description: "ce soir")
 Checklist.create!(title: "Faire la caisse", description: "demain")
 Checklist.create!(title: "Faire la tondeuse", description: "now")
-
 puts "#{Checklist.count}"
