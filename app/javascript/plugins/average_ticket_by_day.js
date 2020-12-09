08f97b37e2a3c84b92d178c944047f4aabcf33b5
Arthur Deleval <arthur.deleval@skema.edu>
2020-12-09 10:26:57 +0100

    JS bug fixes

--- app/javascript/plugins/average_ticket_by_day.js
+++ app/javascript/plugins/average_ticket_by_day.js
@@ -2,24 +2,25 @@ import Chart from 'chart.js';
 Chart.platform.disableCSSInjection = true;
 
 var ctx = document.getElementById('average-ticket');
-var averageTicketChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-    datasets: [
-      {
-        label: 'point',
-        data: [10, 20, 30, 40, 20, 100, 60],
-        backgroundColor: 'transparent',
-        borderColor: '#34D399',
-        pointBackgroundColor: '#F3A4AC',
-        pointBorderColor: 'circle',
-        pointBorderWidth: 20,
-        pointStyle: '#F3A4AC',
-        borderWidth: 5
-      },
-    ]
-  }
-});
-
+if(ctx){
+  var averageTicketChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+      datasets: [
+        {
+          label: 'point',
+          data: [10, 20, 30, 40, 20, 100, 60],
+          backgroundColor: 'transparent',
+          borderColor: '#34D399',
+          pointBackgroundColor: '#F3A4AC',
+          pointBorderColor: 'circle',
+          pointBorderWidth: 20,
+          pointStyle: '#F3A4AC',
+          borderWidth: 5
+        },
+      ]
+    }
+  });
+}
 export {averageTicketChart};
